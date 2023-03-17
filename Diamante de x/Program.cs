116b565dbc55cc47445d81a37982fd676029bcf4
tomat <tomat@DESKTOP-0VULJ7N>
2023-03-17 14:04:37 -0300

    Add project files.

---
+++ Diamante de x/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Diamante_de_x
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número ímpar: ");
+            int num = int.Parse(Console.ReadLine());
+
+            
+            for (int i = 0; i < num; i++)
+            {
+                int numeroX, numeroEspaco;
+                if (i <= num / 2)
+                {
+                    numeroX = 2 * i + 1;
+                    numeroEspaco = (num - 1 - numeroX) / 2;
+                }
+                else
+                {
+                    numeroX = 2 * (num - i) - 1;
+                    numeroEspaco = (num - 1 - numeroX) / 2;
+                }
+                for (int j = 0; j < numeroEspaco; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < numeroX; j++)
+                {
+                    Console.Write("x");
+                }
+                for (int j = 0; j < numeroEspaco; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
